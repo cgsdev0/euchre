811259ba5d9f94441001cdb3694f20bb333f9374
Sarah Schulte <git@sarah.engineer>
2026-02-27 20:14:29 -0800

    yay typegen

--- game/src/api/API.cpp
+++ game/src/api/API.cpp
@@ -1455,21 +1455,21 @@ namespace nlohmann {
     }
 }
 namespace API {
-std::string Achievement::toString() const {
-json j;
-to_json(j, *this);
-return j.dump();
-}
-void Achievement::fromString(const std::string &s) {
-auto j = json::parse(s);
-from_json(j, *this);
-}
 std::string AchievementData::toString() const {
 json j;
 to_json(j, *this);
 return j.dump();
 }
 void AchievementData::fromString(const std::string &s) {
+auto j = json::parse(s);
+from_json(j, *this);
+}
+std::string Achievement::toString() const {
+json j;
+to_json(j, *this);
+return j.dump();
+}
+void Achievement::fromString(const std::string &s) {
 auto j = json::parse(s);
 from_json(j, *this);
 }
