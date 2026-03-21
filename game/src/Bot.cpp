4ee21614e9d71480b31ed1db0e853435be28c0ce
Brian Chrzanowski <chrzanowskitest@gmail.com>
2026-03-21 18:36:33 -0400

    wip

--- game/src/Bot.cpp
+++ game/src/Bot.cpp
@@ -59,6 +59,20 @@ namespace Bot {
         return suit == Suit::DIAMONDS || suit == Suit::HEARTS ? 0 : 1;
     }
 
+    static int score_card(Card card, Suit trump) {
+        if (color(card.suit) == color(trump)) {
+            if (card.rank == Rank::JACK) {
+                return card.suit == trump ? 19 : 18;
+            } else {
+            }
+        } else {
+            return (int)card.rank;
+        }
+    }
+
+    static int score_hand_for_suit(std::vector<Card> hand, Suit trump) {
+    }
+
     bool orderTrump(BotDecisionState &state, std::optional<Suit> &suit, bool &go_alone) {
         assert(state.hand.size() == 5);
         assert(state.top_card.has_value());
@@ -66,61 +80,39 @@ namespace Bot {
         suit = std::nullopt;
 
         if (state.phase == Phase::VOTE_ROUND1) {
-            // Simple heuristic to start, if we have three trump, or two trump and a bower, we call
-            // the suit.
-
-            int trump_count = 0;
-            int bower_count = 0;
-            int off_aces = 0;
-
-            for (size_t i = 0; i < state.hand.size(); i++) {
-                Card card = state.hand[i];
-
-                bool is_trump_color = color((*state.top_card).suit) == color(card.suit);
-
-                bool is_bower = card.rank == Rank::JACK && is_trump_color;
-                if (is_bower) {
-                    bower_count++;
-                }
-
-                if (is_trump_color && card.rank >= Rank::JACK) {
-                    trump_count++;
-                }
-
-                if (!is_trump_color && card.rank == Rank::ACE) {
-                    off_aces++;
-                }
-            }
-
-            bool order_up =
-                (trump_count >= 3) ||
-                (trump_count >= 2 && bower_count >= 1) ||
-                (trump_count >= 2 && off_aces >= 1);
-
-            if (order_up) {
-                suit = (*state.top_card).suit;
-                if (bower_count == 2)
-                    go_alone = true;
-            }
-
-            return order_up;
+            int score = score_for_suit(state.hand, (*state.top_card).suit);
         } else if (state.phase == Phase::VOTE_ROUND2) {
-        } else {
+            int score = score_for_suit(state.hand, (*state.top_card).suit);
         }
 
         return false;
-    }
-
-    Card playCard(BotDecisionState state) {
-        // just play the first card in the hand
-        Card card = state.hand[0];
-        state.hand.erase(state.hand.begin());
-        return card;
     }
 
     Card discard(BotDecisionState state) {
         Card card = state.hand[0];
         state.hand.erase(state.hand.begin());
         return card;
+    }
+
+    static std::vector<Card> sort_hands_by_value(std::vector<Card> cards, Suit trump) {
+        auto copy = cards;
+        std::sort(copy.begin(), copy.end(), [&](Card a, Card b){
+            return score_card(a, trump) < score_card(b, trump);
+        });
+        return cards;
+    }
+
+    bool has_suit(std::vector<Card> &cards, Suit check) {
+    }
+
+    Card playCard(BotDecisionState state) {
+        auto sorted_cards = sort_hands_by_value(state.hand, state.trump);
+        if (state.stack.size() == 0) { // leading
+            return sorted_cards[0];
+        } else {
+            Card led = state.stack[0];
+            if (has_suit(sorted_cards, led.suit)) {
+            }
+        }
     }
 }
